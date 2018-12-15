342f883c97cd9be9a90e62e21af1a1134c59fd26
jtparrett <jtparrett@me.com>
2018-12-15 16:35:26 +0000

    initial build

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styles from 'styled-components'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+import './index.css'
+
+import Logo from './assets/logo.svg'
+import Etsy from './assets/etsy.png'
+
+const LogoImg = styles.img`
+  display: block;
+  width: 100%;
+  margin-bottom: 50px;
+`
+
+const Wrapper = styles.div`
+  width: 300px;
+  max-width: 100%;
+  margin: 100px auto;
+  text-align: center;
+  padding: 0 20px;
+  box-sizing: border-box;
+`
+
+const Button = styles.a`
+  text-transform: uppercase;
+  padding: 10px 40px;
+  font-size: 12px;
+  color: #000;
+  display: inline-flex;
+  align-items: center;
+  font-weight: bold;
+  border-radius: 200px;
+  border: 2px solid #eaeaea;
+
+  &:hover {
+    background: #eaeaea;
   }
-}
+`
 
-export default App;
+const EtsyLogo = styles.img`
+  width: 40px;
+  margin-left: 8px;
+  display: inline-block;
+`
+
+export default () => (
+  <Wrapper>
+    <LogoImg src={Logo} />
+    <Button href="https://etsy.com/uk/shop/FerreiraBoutiqueCo" target="_blank">Shop on <EtsyLogo src={Etsy} /></Button>
+  </Wrapper>
+)
